095c43e70cff78c01bae479a5e274a9812f1201c
cinderrblock <103962472+cinderrblock@users.noreply.github.com>
2024-05-09 18:03:46 -0300

    deleted all the old bgdps levels, possibly added the tier stat, added mathaclysm

--- js/pages/List.js
+++ js/pages/List.js
@@ -54,6 +54,10 @@ export default {
                             <div class="type-title-sm">Password</div>
                             <p>{{ level.password || 'Free to Copy' }}</p>
                         </li>
+                        <li>
+                            <div class="type-title-sm">Tier</div>
+                            <p>{{ level.tier }}</p>
+                        </li>
                     </ul>
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 150"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
